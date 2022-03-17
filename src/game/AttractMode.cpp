3dd65bbc6cffb2bc1d400208a02ff3a34463188c
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2022-03-17 15:07:53 +0000

    Changed name, added better Attract Mode

--- src/game/AttractMode.cpp
+++ src/game/AttractMode.cpp
@@ -21,4 +21,27 @@ void AttractMode::render() {
 
 
 	Framework::SDLUtils::SDL_RenderDrawArcInWindow(graphics_objects->graphics_ptr->get_renderer(), { WINDOW::SIZE_HALF.x, WINDOW::SIZE.y * 0.7f + radius }, radius, WINDOW::SIZE, COLOURS::PLANETS[earth]);
+
+	float scale = GAME::SANDBOX::UI::DEFAULT_CAMERA_SCALE;
+
+	std::vector<Framework::vec2> vertices;
+	for (const PhysicsEngine::phyvec& v : GAME::COMPONENTS::VERTICES[0]) {
+		vertices.push_back(PhysicsEngine::to_fvec(scale * (v + PhysicsEngine::phyvec{ 0.0, 6.4 }) + WINDOW::SIZE_HALF));
+	}
+
+	graphics_objects->graphics_ptr->render_poly(vertices, COLOURS::WHITE);
+
+	vertices.clear();
+	for (const PhysicsEngine::phyvec& v : GAME::COMPONENTS::VERTICES[1]) {
+		vertices.push_back(PhysicsEngine::to_fvec(scale * (v + PhysicsEngine::phyvec{ 0.0, 17.7 }) + WINDOW::SIZE_HALF));
+	}
+
+	graphics_objects->graphics_ptr->render_poly(vertices, COLOURS::WHITE);
+
+	vertices.clear();
+	for (const PhysicsEngine::phyvec& v : GAME::COMPONENTS::VERTICES[2]) {
+		vertices.push_back(PhysicsEngine::to_fvec(scale * (v + PhysicsEngine::phyvec{ 0.0, 29.4 }) + WINDOW::SIZE_HALF));
+	}
+
+	graphics_objects->graphics_ptr->render_poly(vertices, COLOURS::WHITE);
 }
